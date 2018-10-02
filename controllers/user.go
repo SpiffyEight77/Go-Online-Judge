932b20772e73e42d131f6452462e010bfefcc5e3
SpiffyEight77 <killer_0248@live.com>
2018-10-02 20:57:04 +0800

    Update user & docs

--- controllers/user.go
+++ controllers/user.go
@@ -1,55 +1,141 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"online-judge/common/errCode"
+	"online-judge/middlewares/jwt"
+	"online-judge/models"
+	"time"
+)
 
 type UserLoginRequest struct {
 	Username string `form:"username" json:"username" biding:"required"`
 	Password string `form:"password" json:"password" biding:"required"`
 }
 
-// @Summary Admin Login
+// @Summary User & Admin Login
 // @Produce json
 // @Router /api/v1/admin/user/login [get]
 func GetUserLogin(c *gin.Context) {
-
+	Response(c, 200, errCode.SUCCESS, nil)
 }
 
-// @Summary Admin Login
+// @Summary User & Admin Login
 // @Produce json
 // @Param username query string true "username"
 // @Param password query string true "password"
 // @Router /api/v1/admin/user/login [post]
 func PostUserLogin(c *gin.Context) {
+	var req UserLoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
 
+	valid := validation.Validation{}
+	ok, _ := valid.Valid(&req)
+
+	data := make(map[string]interface{})
+	code := errCode.BADREQUEST
+
+	if ok {
+		if isExist, data := models.CheckAuth(req.Username, req.Password); isExist == true {
+			if token, err := jwt.GenerateToken(req.Username, req.Password); err != nil {
+				code = errCode.UNAUTHORIZED
+			} else {
+				data.Token = token
+				code = errCode.SUCCESS
+				models.UpdateUserLogin(token, time.Now())
+			}
+		} else {
+			code = errCode.UNAUTHORIZED
+		}
+	}
+	Response(c, code, code, data)
+}
+
+type UserRegisterRequest struct {
+	Username string `form:"username" json:"username" biding:"required"`
+	Email    string `form:"email" json:"email" biding:"required"`
+	Password string `form:"password" json:"password" biding:"required"`
 }
 
 // @Summary User Register
 // @Produce json
 // @Router /api/v1/user/login [get]
 func GetUserRegister(c *gin.Context) {
-
+	Response(c, http.StatusOK, errCode.SUCCESS, nil)
 }
 
-// @Summary User Login
+// @Summary User Register
 // @Produce json
 // @Param username query string true "username"
 // @Param password query string true "password"
 // @Router /api/v1/user/login [post]
 func PostUserRegister(c *gin.Context) {
+	req := UserRegisterRequest{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
 
+	if ok, data := models.CheckAuth(req.Username, req.Password); ok || data != nil {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+
+	token, _ := jwt.GenerateToken(req.Username, req.Password)
+	if err := models.Register(req.Username, req.Password, req.Email, token); err == nil {
+		if ok, data := models.CheckAuth(req.Username, req.Password); ok {
+			Response(c, http.StatusOK, errCode.SUCCESS, data)
+			return
+		}
+	}
+	Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+}
+
+type UserProfileRequest struct {
+	Uid      int    `form:"uid" json:"uid" biding:"required"`
+	Username string `form:"username" json:"username"`
+	Password string `form:"password" json:"password"`
+	Email    string `form:"email" json:"email"`
 }
 
 // @Summary User Profile
 // @Produce json
+// @Param uid query int true "uid"
 // @Router /api/v1/user/profile [get]
 func GetUserProfile(c *gin.Context) {
-
+	req := UserProfileRequest{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+	if err, data := models.UserProfile(req.Uid); err == nil {
+		Response(c, http.StatusOK, errCode.SUCCESS, data)
+	} else {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+	}
+	Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
 }
 
-// @Summary User Login
+// @Summary User Profile
 // @Produce json
 // @Param uid query int true "uid"
 // @Router /api/v1/user/profile [post]
 func PostUserProfile(c *gin.Context) {
+	req := UserProfileRequest{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
 
+	if err := models.UpdateProfile(req); err == nil {
+		Response(c, http.StatusOK, errCode.SUCCESS, nil)
+	} else {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+	}
+	Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
 }
--- models/user.go
+++ models/user.go
@@ -1,15 +1,44 @@
 package models
 
 import (
+	"online-judge/controllers"
 	"time"
 )
 
 type User struct {
 	ID        int       `gorm:"column:id" json:"uid"`
 	Username  string    `gorm:"column:username" json:"username"`
-	NickName  string    `gorm:"column:nickname" json:"nickname"`
+	Email     string    `gorm:"column:email" json:"email"`
 	Password  string    `gorm:"column:password" json:"password"`
 	Token     string    `gorm:"column:token" json:"token"`
 	CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
 	LastLogin time.Time `gorm:"column:last_login" json:"last_login"`
+}
+
+func CheckAuth(username, password string) (bool, *User) {
+	var user User
+	db.Model(&user).Where(User{Username: username, Password: password}).First(&user)
+	if user.ID > 0 {
+		return true, &user
+	}
+	return false, nil
+}
+
+func UpdateUserLogin(token string, lastLogin time.Time) error {
+	var user User
+	return db.Model(&user).Update(User{Token: token, LastLogin: lastLogin}).Error
+}
+
+func Register(username, password, email, token string) error {
+	user := User{Username: username, Password: password, Email: email, Token: token, CreatedAt: time.Now(), LastLogin: time.Now()}
+	return db.Model(&user).Create(&user).Error
+}
+
+func UserProfile(id int) (error, *User) {
+	var user User
+	return db.Model(&user).Where("id = ?", id).Scan(&user).Error, &user
+}
+
+func UpdateProfile(user controllers.UserProfileRequest) error {
+	return db.Model(&user).Update().Error
 }
