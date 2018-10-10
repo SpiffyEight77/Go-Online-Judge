dfb38ba6ff1354d02e4d0141b00105e8602ce25f
SpiffyEight77 <killer_0248@live.com>
2018-10-10 20:54:29 +0800

    update news

--- controllers/news.go
+++ controllers/news.go
@@ -1,1 +1,110 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"online-judge/common/errCode"
+	"online-judge/models"
+	"strconv"
+)
+
+// @Summary News List
+// @Produce json
+// @Router /api/v1/news/list [get]
+func GetNewsList(c *gin.Context) {
+	data, err := models.NewsList()
+	if err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, data)
+}
+
+// @Summary  News Detail
+// @Produce json
+// @Param news_id query int true "news_id"
+// @Router /api/v1/news/detail [get]
+func GetNewsDetail(c *gin.Context) {
+	nid := c.Query("news_id")
+	if nid == "" {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+
+	newsID, err := strconv.Atoi(nid)
+	if err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+
+	data, err := models.NewsDetail(newsID)
+	if err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, data)
+}
+
+type NewsEditRequest struct {
+	Content string `form:"column:content" json:"content" biding:"required"`
+}
+
+// @Summary  News Edit
+// @Produce json
+// @Param content query string true "content"
+// @Router /api/v1/news/edit [post]
+func PostNewsEdit(c *gin.Context) {
+	req := NewsEditRequest{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+
+	if err := models.NewsUpdate(req); err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, nil)
+}
+
+// @Summary  News Create
+// @Produce json
+// @Param content query string true "content"
+// @Router /api/v1/news/create [post]
+func PostNewsCreate(c *gin.Context) {
+	req := NewsEditRequest{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+
+	if err := models.NewsCreate(req); err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, nil)
+}
+
+// @Summary  News Delete
+// @Produce json
+// @Param news_id query int true "news_id"
+// @Router /api/v1/news/delete [post]
+func PostNewsDelete(c *gin.Context) {
+	nid := c.Query("news_id")
+	if nid == "" {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+
+	newsID, err := strconv.Atoi(nid)
+	if err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+
+	if err := models.NewsDelete(newsID); err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, nil)
+}
--- models/news.go
+++ models/news.go
@@ -1,1 +1,31 @@
 package models
+
+import "time"
+
+type News struct {
+	ID        int       `form:"column:id" json:"id"`
+	Content   string    `form:"column:content" json:"content"`
+	CreatedAt time.Time `form:"column:created_at" json:"created_at"`
+}
+
+func NewsList() (*[]News, error) {
+	var newsList []News
+	return &newsList, db.Model(&News{}).Scan(&newsList).Error
+}
+
+func NewsDetail(newsID int) (News, error) {
+	var news News
+	return news, db.Model(&News{}).Where(&News{ID: newsID}).Scan(&news).Error
+}
+
+func NewsUpdate(news interface{}) error {
+	return db.Model(&News{}).Update(&news).Error
+}
+
+func NewsCreate(news interface{}) error {
+	return db.Model(&News{}).Create(&news).Error
+}
+
+func NewsDelete(newsID int) error {
+	return db.Model(&News{}).Delete(&News{ID: newsID}).Error
+}
