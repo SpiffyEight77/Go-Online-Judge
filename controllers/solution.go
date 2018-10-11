b332539ec18e636d8c4deaf2bbd85cfa8820f444
SpiffyEight77 <killer_0248@live.com>
2018-10-11 15:33:13 +0800

    Update solution

--- controllers/solution.go
+++ controllers/solution.go
@@ -1,1 +1,46 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"online-judge/common/errCode"
+	"online-judge/models"
+	"strconv"
+)
+
+// @Summary Solution List
+// @Produce json
+// @Router /api/v1/solution/list [get]
+func GetSolutionList(c *gin.Context) {
+	data, err := models.SolutionList()
+	if err != nil {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, data)
+}
+
+// @Summary Solution List
+// @Produce json
+// @Param solution_id query int true "solution_id"
+// @Router /api/v1/solution/detail [get]
+func GetSolutionDetail(c *gin.Context) {
+	sid := c.Query("solution_id")
+	if sid == "" {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+
+	solutionID, err := strconv.Atoi(sid)
+	if err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+
+	data, err := models.SolutionDetail(solutionID)
+	if err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, data)
+}
--- models/solution.go
+++ models/solution.go
@@ -1,1 +1,26 @@
 package models
+
+type Solution struct {
+	SID      int    `gorm:"column:sid" json:"sid"`
+	PID      int    `gorm:"column:pid" json:"pid"`
+	UID      int    `gorm:"column:uid" json:"uid"`
+	Judge    int    `gorm:"column:judge" json:"judge"`
+	Code     string `gorm:"column:code" json:"code"`
+	Memory   int    `gorm:"column:memory" json:"memory"`
+	Language int    `gorm:"column:language" json:"language"`
+	Status   int    `gorm:"column:status" json:"status"`
+}
+
+func SubmitProblem(solution interface{}) error {
+	return db.Model(&Solution{}).Create(&solution).Error
+}
+
+func SolutionList() (*[]Solution, error) {
+	var solutionList []Solution
+	return &solutionList, db.Model(&Solution{}).Scan(&solutionList).Error
+}
+
+func SolutionDetail(solutionID int) (*Solution, error) {
+	var solution Solution
+	return &solution, db.Model(&Solution{}).Where(&Solution{SID: solutionID}).Scan(&solution).Error
+}
