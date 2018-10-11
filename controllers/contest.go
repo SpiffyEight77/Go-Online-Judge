6d4b309551ad40bbfd0c8f14973978db92b819ae
SpiffyEight77 <killer_0248@live.com>
2018-10-11 11:51:50 +0800

    update contest

--- controllers/contest.go
+++ controllers/contest.go
@@ -1,7 +1,129 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"online-judge/common/errCode"
+	"online-judge/models"
+	"strconv"
+	"time"
+)
 
-func GetContest(c *gin.Context) {
+// @Summary  Contest List
+// @Produce json
+// @Router /api/v1/contest/list [get]
+func GetContestList(c *gin.Context) {
+	data, err := models.ContestList()
+	if err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, data)
+}
 
+// @Summary  Contest Detail
+// @Produce json
+// @Param contest_id query int true "contest_id"
+// @Router /api/v1/contest/detail [get]
+func GetContestDetail(c *gin.Context) {
+	cid := c.Query("contest_id")
+	if cid == "" {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+
+	contestID, err := strconv.Atoi(cid)
+	if err != nil {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+
+	data, err := models.ContestDetail(contestID)
+	if err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, data)
+}
+
+type ContestCreateRequest struct {
+	Title       string    `form:"title" json:"title" biding:"required"`
+	UID         int       `form:"uid" json:"uid"  biding:"required"`
+	StartAt     time.Time `form:"start_at" json:"start_at" biding:"required"`
+	EndAt       time.Time `form:"end_at" json:"end_at" biding:"required"`
+	Status      int       `form:"status" json:"status" biding:"required"`
+	ProblemNum  int       `form:"problem_num" json:"problem_num" biding:"required"`
+	Participant int       `form:"participant" json:"participant" biding:"required"`
+}
+
+// @Summary  Contest Create
+// @Produce json
+// @Param title query string true "title"
+// @Param uid query int true "uid"
+// @Param start_at query time true "start_at"
+// @Param end_at query time true "end_at"
+// @Param status query int true "status"
+// @Param problem_num query int true "problem_num"
+// @Param participant query int true "participant"
+// @Router /api/v1/admin/contest/create [post]
+func PostCreateContest(c *gin.Context) {
+	req := ContestCreateRequest{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+
+	if err := models.ContestCreate(req); err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, nil)
+}
+
+// @Summary  Contest Detail
+// @Produce json
+// @Param contest_id query int true "contest_id"
+// @Router /api/v1/admin/contest/delete [post]
+func PostDeleteContest(c *gin.Context) {
+	cid := c.Query("contest_id")
+	if cid == "" {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+
+	contestID, err := strconv.Atoi(cid)
+	if err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+
+	if err := models.ContestDelete(contestID); err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, nil)
+}
+
+// @Summary  Contest Edit
+// @Produce json
+// @Param title query string true "title"
+// @Param uid query int true "uid"
+// @Param start_at query time true "start_at"
+// @Param end_at query time true "end_at"
+// @Param status query int true "status"
+// @Param problem_num query int true "problem_num"
+// @Param participant query int true "participant"
+// @Router /api/v1/admin/contest/edit [post]
+func PostUpdateContest(c *gin.Context) {
+	req := ContestCreateRequest{}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		Response(c, http.StatusBadRequest, errCode.BADREQUEST, nil)
+		return
+	}
+
+	if err := models.ContestUpdate(req); err != nil {
+		Response(c, http.StatusInternalServerError, errCode.ERROR, nil)
+		return
+	}
+	Response(c, http.StatusOK, errCode.SUCCESS, nil)
 }
--- models/contest.go
+++ models/contest.go
@@ -1,1 +1,37 @@
 package models
+
+import "time"
+
+type Contest struct {
+	ID          int       `gorm:"column:id" json:"id"`
+	Title       string    `gorm:"column:title" json:"title"`
+	UID         int       `gorm:"column:uid" json:"uid"`
+	CreatedAt   time.Time `gorm:"column:created_at" json:"created_at"`
+	StartAt     time.Time `gorm:"column:start_at" json:"start_at"`
+	EndAt       time.Time `gorm:"column:end_at" json:"end_at"`
+	Status      int       `gorm:"column:status" json:"status"`
+	ProblemNum  int       `gorm:"column:problem_num" json:"problem_num"`
+	Participant int       `gorm:"column:participant" json:"participant"`
+}
+
+func ContestList() (*[]Contest, error) {
+	var contestList []Contest
+	return &contestList, db.Model(&Contest{}).Scan(&contestList).Error
+}
+
+func ContestDetail(contestID int) (*Contest, error) {
+	var contest Contest
+	return &contest, db.Model(&Contest{}).Where(&Contest{ID: contestID}).Scan(&contest).Error
+}
+
+func ContestCreate(contest interface{}) error {
+	return db.Model(&Contest{}).Create(&contest).Error
+}
+
+func ContestDelete(contestID int) error {
+	return db.Model(&Contest{}).Delete(&Contest{ID: contestID}).Error
+}
+
+func ContestUpdate(contest interface{}) error {
+	return db.Model(&Contest{}).Update(&contest).Error
+}
